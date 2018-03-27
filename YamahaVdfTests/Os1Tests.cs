e8c9ad8622ac73f1fb86b67aecf7fe73a0c4c66b
Adrian Demavivas <adrian.demavivas@gmail.com>
2018-03-26 22:20:08 -0500

    Added test - Test if OS1 string is valid - Test for invalid indicator

--- YamahaVdfTests/Os1Tests.cs
+++ YamahaVdfTests/Os1Tests.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using YamahaVdf;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace YamahaVdf.Tests
 {
@@ -15,13 +11,13 @@ namespace YamahaVdf.Tests
         {
             const string CONTENT =
 @"
-A,7523100,82720,Fall Order,FALL2016,W40,P,R,D,1251510,YMUS,G
+A,7523100,82720,Fall Order,FALL2016,W40,P,R,D,1251510,YMUS,G,004
 B,7523100,Flora Garibian,123 Main,,,,,,,,
 C,7523100,Flora's Place,HURST,TX,780069218,US,2106068788,,,,
 G,7523100,111-111-1111,111-111-1111,,,,,,,,
 D,7523100,240,SXS14KIT0103,1,0.01,20161202,576,,,,
 D,7523100,250,SXS14KIT0103,1,0.01,20161202,576,,,,
-A,7535444,53100,Fall,FALL2016,W30,P,R,D,1251510,YMUS,G
+A,7535444,53100,Fall,FALL2016,W30,P,R,D,1251510,YMUS,G,009
 B,7535444,Stephen Brown,123 Main,,,,,,,,
 C,7535444,Browns Place,MANSFIELD,TX,275378269,US,2524928553,,,,
 G,7535444,111-111-1111,111-111-1111,,,,,,,,
