f13deebd171390c2e4fec596aa40fc512c9e3850
Adrian Demavivas <adrian.demavivas@gmail.com>
2018-04-07 20:13:57 -0500

    Add Detail class to store the file contents.  The OS2 file could contain multiple lines of records.

--- YamahaVdf/Os2.cs
+++ YamahaVdf/Os2.cs
@@ -1,36 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace YamahaVdf
-{
+{ 
     public class Os2
     {
-        public Os2(string content)
+        public class Detail
         {
-            if (String.IsNullOrEmpty(content))
-                throw new ArgumentNullException(nameof(content), "Content cannot be an empty string");
+            public string SupplierCode { get; set; }
+            public double SalesOrderNo { get; set; }
+            public int SalesOrderLineNo { get; set; }
+            public string PartNo { get; set; }
+            public DateTime ConfirmedReceiptDate { get; set; }
+            public double AllocatedQuantity { get; set; }
+            public DateTime ExpectedShipDate { get; set; }
 
-            var contents =
-                content
-                .Split(new[] { Constants.DELIMITER }, StringSplitOptions.None)
-                .ToArray();
+            internal Detail(string content)
+            {
+                if(String.IsNullOrEmpty(content))
+                    throw new ArgumentNullException(nameof(content), "Content cannot be an empty string");
 
-            int index = 0;
-            SupplierCode = contents[index++];
-            SalesOrderNo = double.Parse(contents[index++]);
-            SalesOrderLineNo = int.Parse(contents[index++]);
-            PartNo = contents[index++];
-            ConfirmedReceiptDate = DateTime.ParseExact(contents[index++], "yyyyMMdd", null);
-            AllocatedQuantity = double.Parse(contents[index++]);
-            ExpectedShipDate = DateTime.ParseExact(contents[index++], "yyyyMMdd", null);
+                var contents =
+                    content
+                    .Split(new[] { Constants.DELIMITER }, StringSplitOptions.None)
+                    .ToArray();
+
+                int index = 0;
+                SupplierCode = contents[index++];
+                SalesOrderNo = double.Parse(contents[index++]);
+                SalesOrderLineNo = int.Parse(contents[index++]);
+                PartNo = contents[index++];
+                ConfirmedReceiptDate = DateTime.ParseExact(contents[index++], "yyyyMMdd", null);
+                AllocatedQuantity = double.Parse(contents[index++]);
+                ExpectedShipDate = DateTime.ParseExact(contents[index++], "yyyyMMdd", null);
+            }
         }
 
-        public string SupplierCode { get; set; }
-        public double SalesOrderNo { get; set; }
-        public int SalesOrderLineNo { get; set; }
-        public string PartNo { get; set; }
-        public DateTime ConfirmedReceiptDate { get; set; }
-        public double AllocatedQuantity { get; set; }
-        public DateTime ExpectedShipDate { get; set; }
+        public Os2(string[] contents)
+        {
+            foreach(var content in contents)
+            {
+                Details.Add(new Detail(content));
+            }
+        }
+
+        public List<Detail> Details { get; } = new List<Detail>();
     }
 }
